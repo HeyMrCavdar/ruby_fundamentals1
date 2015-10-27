52e18e8d9b044ed3e914e2da7c28f695e139b0ea
Matt <matt.cavdar@gmail.com>
2015-10-27 14:50:32 -0400

    create exercise 4

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,6 @@
 puts "What is your name?"
 name = gets.strip #strip vs chomp because it removes white space at beginning and end
-puts "Hi #{name}!"
+puts "How old are you?"
+age = gets.strip.to_i
+year = 2015 - age
+puts "Hi #{name}! You were born in #{year}"
