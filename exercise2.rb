3dce37e541d94f482bab6e1cb8886383e9a33b0e
Matt <matt.cavdar@gmail.com>
2015-10-27 14:17:51 -0400

    Upload exercise2 completed

---
+++ exercise2.rb
@@ -0,0 +1,14 @@
+#leave a 15% tip on a 55$ meal
+number = 55* 0.15
+	puts "$" + "#{number}"
+
+#Adding a string and an integer using .to_s method (to string)
+puts "Magnificent" + 7.to_s
+
+#multiplication by string interpolation
+a = 45628
+b = 7839
+puts "The value of 45628 multiplied by 7839 is #{a*b}"
+
+#Boolean operators, will lead to true
+puts (true && false) || (false && true) || !(false && false)
